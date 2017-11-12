d26a91f07441b824a3afc5ee2dbb7f20955da9af
Arnaud Ha <arnaud752037@gmail.com>
2017-11-12 16:41:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,23 @@
-echo "Starting install..."
+echo " "
+echo "  _       _ _   _____       _     "
+echo " (_)     (_) | |  __ \     | |    "
+echo "  _ _ __  _| |_| |  | | ___| |__  "
+echo " | | '_ \| | __| |  | |/ _ \ '_ \ "
+echo " | | | | | | |_| |__| |  __/ |_) |"
+echo " |_|_| |_|_|\__|_____/ \___|_.__/ "
+echo " "
+echo "initDeb script v1"
+echo " "
+echo "Starting Update..."
 apt-get update
-apt-get -y upgrade
-apt-get dist-upgrade
-apt-get install sudo
-apt-get install htop
+echo "Starting Upgrade..."
+apt-get upgrade -y
+apt-get dist-upgrade -y
+echo "Starting install sudo..."
+apt-get install -y sudo
+echo "Starting install htop..."
+apt-get install -y htop
+echo "Starting install Apache..."
 apt-get install -y apache2
-apt install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-mbstring php7.0-xml php7.0-zip -Y
+echo "Starting install php & dependencies..."
+apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-mbstring php7.0-xml php7.0-zip -Y
