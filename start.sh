82316e1ea4a72f15c438877159cf9833de383e45
Arnaud Ha <arnaud752037@gmail.com>
2017-11-12 16:58:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,6 +13,8 @@ apt-get update
 echo "Starting Upgrade..."
 apt-get upgrade -y
 apt-get dist-upgrade -y
+echo "Starting install dialog..."
+apt-get install -y dialog
 echo "Starting install sudo..."
 apt-get install -y sudo
 echo "Starting install htop..."
@@ -20,4 +22,6 @@ apt-get install -y htop
 echo "Starting install Apache..."
 apt-get install -y apache2
 echo "Starting install php & dependencies..."
-apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-mbstring php7.0-xml php7.0-zip -Y
+apt-get install -y php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-mbstring php7.0-xml php7.0-zip
+echo "Starting install mysql-server..."
+apt-get install -y mysql-server
